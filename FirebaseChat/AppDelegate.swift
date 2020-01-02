f779255210faa61442dc3e341d80aed266ed67f0
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-01-02 19:17:41 +0100

    first commit

---
+++ FirebaseChat/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  FirebaseChat
+//
+//  Created by Alan Casas on 02/01/2020.
+//  Copyright © 2020 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .cyan
+        let navVC = UINavigationController(rootViewController: viewController)
+        
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {}
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+    
+    func applicationWillTerminate(_ application: UIApplication) {}
+}
+
